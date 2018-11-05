7c25b6a1201bd37879ae6375cb36d03fc89e98d7
Sam Kreter <samkreter@gmail.com>
2018-11-04 18:46:28 -0800

    small changes

--- irisTrain/pytrain.py
+++ irisTrain/pytrain.py
@@ -13,7 +13,6 @@ parser.add_argument('gammaparam', type=float, help='Parameter Gamma for SVM')
 args = parser.parse_args()
 
 print("OUTDIR: ", args.outdir)
-print("INDIR: ", args.indir)
 
 # training set column names
 cols = [
